fce8659dbf64186d1da1fa0d139d5c55a4713a8b
Nuker0s <nuker0s2@gmail.com>
2023-12-19 19:14:08 +0100

    working on enemies

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,19 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
-   
+    public float damage;
+    public float playerdetecionrange = 60;
+    public float attackrange;
+    public float attackcooldown;
+    public Transform target;
+    public NavMeshAgent agent;
+    public Vector3 targetpos;
+    
+
     // Start is called before the first frame update
-    void Start()
+    public virtual void Start()
     {
-        
+        target = GameObject.Find("Bob").transform;
     }
 
     // Update is called once per frame
-    void Update()
+    public virtual void Update()
     {
-        
+
+
+    }
+    public virtual void navigation() 
+    {
+        if (Vector3.Distance(target.position, transform.position) < playerdetecionrange)
+        {
+            if (Vector3.Distance(target.position, transform.position) < attackrange)
+            {
+                agent.isStopped = true;
+                StartCoroutine(attack());
+            }
+            else
+            {
+                agent.isStopped = false;
+                StopCoroutine(attack());
+                if (target.position != targetpos)
+                {
+                    targetpos = target.position;
+                    agent.SetDestination(targetpos);
+
+
+                }
+            }
+
+        }
+    }
+    public virtual IEnumerator attack() 
+    {
+        yield return null;
     }
 }
--- Assets/Scripts/basicenemy.cs
+++ Assets/Scripts/basicenemy.cs
@@ -3,29 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class basicenemy : MonoBehaviour
+public class basicenemy : Enemy
 {
-    public Transform target;
-    public NavMeshAgent agent;
-    public Vector3 targetpos;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (target.position != targetpos) 
-        {
-            targetpos = target.position;
-            agent.SetDestination(targetpos);
-            
-
-        }
-        
-    }
+    public float chompcasttime;
+    public float chomprange;
     private void OnDrawGizmos()
     {
         foreach (var item in agent.path.corners)
@@ -33,5 +14,9 @@ public class basicenemy : MonoBehaviour
             Gizmos.color = Color.red;
             Gizmos.DrawSphere(item, 1f);
         }
+    }
+    public override IEnumerator attack()
+    {
+        yield return new WaitForSeconds(chompcasttime);
     }
 }
