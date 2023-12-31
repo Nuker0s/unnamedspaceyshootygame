4cf97f3b22bd6a8e160533db3b59f4cbe20e2dae
Nuker0s <nuker0s2@gmail.com>
2023-12-31 16:25:00 +0100

    working on mainmenu and options

--- Assets/Scripts/mainmenu.cs
+++ Assets/Scripts/mainmenu.cs
@@ -1,26 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-public class mainmenu : MonoBehaviour
+
+public class MainMenu : MonoBehaviour
 {
 
     // Start is called before the first frame update
-    private void Start()
+    void Start()
     {
-
+        
     }
-    public static float initorloadplayerprefsvalves(string name,float value) 
+
+    // Update is called once per frame
+    void Update()
     {
-        if (PlayerPrefs.HasKey(name))
-        {
-            return PlayerPrefs.GetFloat(name);
-            
-        }
-        else
-        {
-            PlayerPrefs.SetFloat(name, value);
-            return value;
-        }
+        
+    }
+    public static void LoadButton(string SceneToLoad) 
+    {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+    public static void ToOtherMenu(GameObject selfparent, GameObject obj) 
+    {
+        obj.SetActive(true);
+        selfparent.gameObject.SetActive(false);
+    }
+    public static void Quit() 
+    {
+        Application.Quit();
     }
 }
